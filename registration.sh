2db0474f0b07453a452873314ec6335b2b3d511b
rrz03 <161994818+rrz03@users.noreply.github.com>
2025-04-09 16:46:57 -0400

    Update registration.sh

--- registration.sh
+++ registration.sh
@@ -4,7 +4,7 @@ module load itk
 moving_image=$1
 fixed_image=$2
 output_tag=$3
-# change the path to your own file directorys
+# change the path to your own file directories
 # moving image is the histology
 # fixed image is the block-face image
 
@@ -14,18 +14,18 @@ neighborhood_size=$6
 
 
 antsRegistration --verbose 1 --dimensionality 2 --float 0 --collapse-output-transforms 1 \
---output [${output_tag}_${sampling}_${neighborhood_size}_${mesh_size_at_base_level},${output_tag}Warped_${sampling}_${neighborhood_size}_${mesh_size_at_base_level}.nii.gz,${output_tag}InverseWarped_${sampling}_${neighborhood_size}_${mesh_size_at_base_level}.nii.gz] \
---interpolation Linear --use-histogram-matching 1 \
---initial-moving-transform [$fixed_image,$moving_image,1] \
---transform Similarity[0.1] \
---metric MeanSquares[$fixed_image,$moving_image,1,$neighborhood_size,Regular,$sampling] \
---convergence [1000x500x250x100,1e-6,10] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox \
---transform Affine[0.1] \
---metric MeanSquares[$fixed_image,$moving_image,1,$neighborhood_size,Regular,$sampling] \
---convergence [1000x500x250x100,1e-6,10] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox \
---transform BSplineSyN[0.1,$mesh_size_at_base_level] \
---metric CC[$fixed_image,$moving_image,1,$neighborhood_size,Regular,$sampling] \
---convergence [50x25x10x10,1e-6,2] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox
+      --output [${output_tag}_${sampling}_${neighborhood_size}_${mesh_size_at_base_level},${output_tag}Warped_${sampling}_${neighborhood_size}_${mesh_size_at_base_level}.nii.gz,${output_tag}InverseWarped_${sampling}_${neighborhood_size}_${mesh_size_at_base_level}.nii.gz] \
+      --interpolation Linear --use-histogram-matching 1 \
+      --initial-moving-transform [$fixed_image,$moving_image,1] \
+      --transform Similarity[0.1] \
+      --metric MeanSquares[$fixed_image,$moving_image,1,$neighborhood_size,Regular,$sampling] \
+      --convergence [1000x500x250x100,1e-6,10] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox \
+      --transform Affine[0.1] \
+      --metric MeanSquares[$fixed_image,$moving_image,1,$neighborhood_size,Regular,$sampling] \
+      --convergence [1000x500x250x100,1e-6,10] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox \
+      --transform BSplineSyN[0.1,$mesh_size_at_base_level] \
+      --metric CC[$fixed_image,$moving_image,1,$neighborhood_size,Regular,$sampling] \
+      --convergence [50x25x10x10,1e-6,2] --shrink-factors 12x8x4x2 --smoothing-sigmas 4x3x2x1vox
 
 # submit with sbatch
 # for mesh_size_at_base_level in {3,5,10,15,20,25,30,35,40,45,50}; do
